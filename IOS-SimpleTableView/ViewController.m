c008726a1d48f1b9bbf57d4a5ef7365009893e95
etyios44 <ety.ios44@gmail.com>
2020-04-11 21:55:17 +0200

    commit with filelist

--- IOS-SimpleTableView/ViewController.m
+++ IOS-SimpleTableView/ViewController.m
@@ -20,7 +20,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+    
+    NSLog(@"\n\nGetting a Directory File Listing\n");
+    
+    NSFileManager *filemgr;
+    NSString *currentpath;
+    NSArray *filelist;
+    int c;
+    int i;
+    
+    filemgr = [NSFileManager defaultManager];
+    
+    filelist = [filemgr contentsOfDirectoryAtPath: @"/tmp" error: nil];
+    
+    c = [filelist count] ;
+    
+    for (i = 0; i < c; i++)
+        NSLog (@"%@", [filelist objectAtIndex: i]);
+    
+    tableData = filelist;
 
 }
 
